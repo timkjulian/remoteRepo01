7bd3152ac7fcba90cd82be0461fd57bf085f9ee4
hvalle <hugo.valle@gmail.com>
2021-08-09 08:58:09 -0600

    Add argparser

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello Waldo")
+#!/usr/bin/env python3
+# Purpose: Say Hello to Waldo
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('name', help='Name to greet')
+args = parser.parse_args()
+print('Hello, ' + args.name + '!')
+
+# print("Hello Waldo")
