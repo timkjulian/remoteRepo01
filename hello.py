412c195132266e5627f9c42d8b012e0e16dc1947
hvalle <hugo.valle@gmail.com>
2021-08-09 09:36:56 -0600

    Make it a module

--- hello.py
+++ hello.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
-# Purpose: Say Hello to Waldo
+"""
+Author: Hugo Valle
+Purpose: Say Hello to Waldo
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
 
-# print("Hello Waldo")
+def get_args():
+    """
+    Get the command-line arguments
+    :return: list of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """
+    This is where the actions is
+    :return: None
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()
