e37a3c4b913d4e405e9ebef37cc74c67fcc795e9
hvalle <hugo.valle@gmail.com>
2021-08-11 13:32:50 -0600

    Initial commit

---
+++ Day3/recaman.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Author : hvalle <me@wsu.com>
+Date   : 8/11/2021
+Purpose: Create a Recaman series: https://en.wikipedia.org/wiki/Recam%C3%A1n%27s_sequence.
+"""
+import sys
+from itertools import count, islice
+
+def sequence():
+    """Recaman sequence:
+        a_0 = 0,
+        a_n = a_(n-1) - n : if result is positive AND not already in the list,
+        a_n = a_(n-1) + n
+        ex: 0, 1, 3, 6, 2, 7, 13, 20, 12, 21, 11, 22, 10, 23,
+        """
+    seen = set()
+    a = 0
+    for n in count(1):
+        yield a
+        seen.add(a)
+        c = a-n
+        if c < 0 or c in seen:
+            c = a+n
+        a = c
+
+
+def write_sequence(filename, num):
+    """
+    Write the Recaman series
+    :param filename: file name
+    :param num: maximum number to write
+    :return: nothing
+    """
+    f = open(filename, mode='wt', encoding='utf-8')
+    # Generate list and Write list
+    f.writelines('{0}\n'.format(r)
+                 for r in islice(sequence(), num + 1))
+    f.close()
+
+
+def read_series(filename):
+    f = open(filename, mode='rt', encoding='utf-8')
+    series = []
+    for line in f:
+        series.append(int(line.strip()))
+    f.close()
+    return series
+
+
+
+# --------------------------------------------------
+def main():
+    """Make your noise here"""
+    write_sequence('recaman.txt', 100)
+    l = read_series('recaman.txt')
+    print(l)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
