d8ecf8792394128a76fcd343e5ca987f9baf623a
hvalle <hugo.valle@gmail.com>
2021-08-12 09:00:02 -0600

    Initial commit

--- Day4/forward_args.py
+++ Day4/forward_args.py
@@ -4,7 +4,7 @@ Author : hvalle <me@wsu.com>
 Date   : 8/12/2021
 Purpose: Forward arguments
 """
-
+from pprint import pp
 
 def trace(f, *args, **kwargs):
     print(f'args={args}')
@@ -19,10 +19,28 @@ def test_trace():
     trace(int, "ff", base=16)
 
 
+def test_tables():
+    # Use built-in zip
+    sunday = [12, 14, 15, 15, 17, 21, 22, 22, 23, 22, 20, 18]
+    monday = [13, 14, 14, 16, 20, 21, 22, 22, 21, 19, 18, 16]
+    tuesday = [12, 13, 14, 16, 20, 20, 21, 20, 19, 16, 14, 12]
+    daily = [sunday, monday, tuesday]
+    pp(daily)
+
+    # for item in zip(sunday, monday, tuesday):
+    for item in zip(*daily):
+            print(item)
+
+    # Create a list of tuples
+    transposed = list(zip(*daily))
+    pp(transposed)
+
+
 # --------------------------------------------------
 def main():
     """Make your noise here"""
-    test_trace()
+    # test_trace()
+    test_tables()
 
 
 # --------------------------------------------------
